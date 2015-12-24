a49c996f62d3609a24cd663d7a93e8a4a81b9442
slidenerd <slidenerd@gmail.com>
2015-12-24 14:59:48 +0530

    Added the toolbar to xml and setSupportActionBar to display the title. The app crashes as of now on Nexus 6P since it runs out of memory trying to display the image

--- app/src/main/java/slidenerd/vivz/bucketdrops/ActivityMain.java
+++ app/src/main/java/slidenerd/vivz/bucketdrops/ActivityMain.java
@@ -1,13 +1,18 @@
 package slidenerd.vivz.bucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
