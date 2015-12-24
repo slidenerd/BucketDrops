e7cb55b34e89399007f227c15ce6077369b987a8
slidenerd <slidenerd@gmail.com>
2015-12-24 16:26:19 +0530

    Added Glide library to display background image and fixed the OutOfMemoryError

--- app/src/main/java/slidenerd/vivz/bucketdrops/ActivityMain.java
+++ app/src/main/java/slidenerd/vivz/bucketdrops/ActivityMain.java
@@ -3,6 +3,9 @@ package slidenerd.vivz.bucketdrops;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -14,5 +17,16 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        initBackgroundImage();
     }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
+    }
+
+
 }
