ef7e4659c80ca8ef6fd81bc7c431ac4cf19c5d91
slidenerd <slidenerd@gmail.com>
2015-12-30 09:22:41 +0530

    Added a drop to realm through DialogAdd and used the RealmBrowser to show the added items

--- app/src/main/java/slidenerd/vivz/bucketdrops/DialogAdd.java
+++ app/src/main/java/slidenerd/vivz/bucketdrops/DialogAdd.java
@@ -11,6 +11,10 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import slidenerd.vivz.bucketdrops.beans.Drop;
+
 /**
  * Created by vivz on 28/12/15.
  */
@@ -24,9 +28,32 @@ public class DialogAdd extends DialogFragment {
     private View.OnClickListener mBtnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            int id =v.getId();
+            switch (id){
+                case R.id.btn_add_it:
+                    addAction();
+                    break;
+            }
             dismiss();
         }
     };
+
+    private void addAction() {
+        //get the value of the 'goal' or 'to-do'
+        //get the time when it was added
+        String what=mInputWhat.getText().toString();
+        long now = System.currentTimeMillis();
+        RealmConfiguration configuration=new RealmConfiguration.Builder(getActivity()).build();
+        Realm.setDefaultConfiguration(configuration);
+        Realm realm=Realm.getDefaultInstance();
+        Drop drop= new Drop(what, now, 0, false);
+        realm.beginTransaction();
+        realm.copyToRealm(drop);
+        realm.commitTransaction();
+        realm.close();
+
+    }
+
     public DialogAdd() {
     }
 
@@ -42,8 +69,9 @@ public class DialogAdd extends DialogFragment {
         mBtnClose = (ImageButton) view.findViewById(R.id.btn_close);
         mInputWhat = (EditText) view.findViewById(R.id.et_drop);
         mInputWhen = (DatePicker) view.findViewById(R.id.bpv_date);
-        mBtnAdd = (Button) view.findViewById(R.id.btn_add);
+        mBtnAdd = (Button) view.findViewById(R.id.btn_add_it);
 
         mBtnClose.setOnClickListener(mBtnClickListener);
+        mBtnAdd.setOnClickListener(mBtnClickListener);
     }
 }
