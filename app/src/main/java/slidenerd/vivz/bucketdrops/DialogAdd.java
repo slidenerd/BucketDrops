0f52fea51d87fd48aca5b530217e8d16bff20c30
slidenerd <slidenerd@gmail.com>
2016-01-10 17:56:47 +0530

    Added the code to link the custom widget in XML to code and display current time inside the Picker

--- app/src/main/java/slidenerd/vivz/bucketdrops/DialogAdd.java
+++ app/src/main/java/slidenerd/vivz/bucketdrops/DialogAdd.java
@@ -7,14 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-import java.util.Calendar;
-
 import io.realm.Realm;
 import slidenerd.vivz.bucketdrops.beans.Drop;
+import slidenerd.vivz.bucketdrops.widgets.BucketPickerView;
 
 /**
  * Created by vivz on 28/12/15.
@@ -25,7 +23,7 @@ public class DialogAdd extends DialogFragment {
 
     private ImageButton mBtnClose;
     private EditText mInputWhat;
-    private DatePicker mInputWhen;
+    private BucketPickerView mInputWhen;
     private Button mBtnAdd;
 
     private View.OnClickListener mBtnClickListener = new View.OnClickListener() {
@@ -45,17 +43,9 @@ public class DialogAdd extends DialogFragment {
         //get the value of the 'goal' or 'to-do'
         //get the time when it was added
         String what = mInputWhat.getText().toString();
-        String date = mInputWhen.getDayOfMonth() + "/" + mInputWhen.getMonth() + "/" + mInputWhen.getYear();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_MONTH, mInputWhen.getDayOfMonth());
-        calendar.set(Calendar.MONTH, mInputWhen.getMonth());
-        calendar.set(Calendar.YEAR, mInputWhen.getYear());
-        calendar.set(Calendar.HOUR, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
         long now = System.currentTimeMillis();
         Realm realm = Realm.getDefaultInstance();
-        Drop drop = new Drop(what, now, calendar.getTimeInMillis(), false);
+        Drop drop = new Drop(what, now, mInputWhen.getTime(), false);
         realm.beginTransaction();
         realm.copyToRealm(drop);
         realm.commitTransaction();
@@ -83,7 +73,7 @@ public class DialogAdd extends DialogFragment {
         super.onViewCreated(view, savedInstanceState);
         mBtnClose = (ImageButton) view.findViewById(R.id.btn_close);
         mInputWhat = (EditText) view.findViewById(R.id.et_drop);
-        mInputWhen = (DatePicker) view.findViewById(R.id.bpv_date);
+        mInputWhen = (BucketPickerView) view.findViewById(R.id.bpv_date);
         mBtnAdd = (Button) view.findViewById(R.id.btn_add_it);
 
         mBtnClose.setOnClickListener(mBtnClickListener);
---
+++ app/src/main/java/slidenerd/vivz/bucketdrops/widgets/BucketPickerView.java
@@ -0,0 +1,73 @@
+package slidenerd.vivz.bucketdrops.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import slidenerd.vivz.bucketdrops.R;
+
+/**
+ * Created by vivz on 10/01/16.
+ */
+public class BucketPickerView extends LinearLayout {
+    private Calendar mCalendar;
+    private TextView mTextDate;
+    private TextView mTextMonth;
+    private TextView mTextYear;
+    private SimpleDateFormat mFormatter;
+
+    public BucketPickerView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BucketPickerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public BucketPickerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        View view = LayoutInflater.from(context).inflate(R.layout.bucket_picker_view, this);
+        mCalendar = Calendar.getInstance();
+        mFormatter = new SimpleDateFormat("MMM");
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mTextDate = (TextView) this.findViewById(R.id.tv_date);
+        mTextMonth = (TextView) this.findViewById(R.id.tv_month);
+        mTextYear = (TextView) this.findViewById(R.id.tv_year);
+        int date = mCalendar.get(Calendar.DATE);
+        int month = mCalendar.get(Calendar.MONTH);
+        int year = mCalendar.get(Calendar.YEAR);
+        update(date, month, year, 0, 0, 0);
+    }
+
+    private void update(int date, int month, int year, int hour, int minute, int second) {
+        mCalendar.set(Calendar.DATE, date);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.HOUR, hour);
+        mCalendar.set(Calendar.MINUTE, minute);
+        mCalendar.set(Calendar.SECOND, second);
+        mTextYear.setText(year + "");
+        mTextDate.setText(date + "");
+        mTextMonth.setText(mFormatter.format(mCalendar.getTime()));
+    }
+
+    public long getTime() {
+        return mCalendar.getTimeInMillis();
+    }
+}
