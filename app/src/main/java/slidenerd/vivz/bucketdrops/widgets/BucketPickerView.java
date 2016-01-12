33aac92af305ef068324c4f100a96b6bca8a0f2d
slidenerd <slidenerd@gmail.com>
2016-01-12 18:29:09 +0530

    Added a touch listener to process motion events on our textviews

--- app/src/main/java/slidenerd/vivz/bucketdrops/widgets/BucketPickerView.java
+++ app/src/main/java/slidenerd/vivz/bucketdrops/widgets/BucketPickerView.java
@@ -2,7 +2,9 @@ package slidenerd.vivz.bucketdrops.widgets;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,12 +17,13 @@ import slidenerd.vivz.bucketdrops.R;
 /**
  * Created by vivz on 10/01/16.
  */
-public class BucketPickerView extends LinearLayout {
+public class BucketPickerView extends LinearLayout implements View.OnTouchListener {
     private Calendar mCalendar;
     private TextView mTextDate;
     private TextView mTextMonth;
     private TextView mTextYear;
     private SimpleDateFormat mFormatter;
+    private String TAG = "VIVZ";
 
     public BucketPickerView(Context context) {
         super(context);
@@ -49,6 +52,9 @@ public class BucketPickerView extends LinearLayout {
         mTextDate = (TextView) this.findViewById(R.id.tv_date);
         mTextMonth = (TextView) this.findViewById(R.id.tv_month);
         mTextYear = (TextView) this.findViewById(R.id.tv_year);
+        mTextDate.setOnTouchListener(this);
+        mTextMonth.setOnTouchListener(this);
+        mTextYear.setOnTouchListener(this);
         int date = mCalendar.get(Calendar.DATE);
         int month = mCalendar.get(Calendar.MONTH);
         int year = mCalendar.get(Calendar.YEAR);
@@ -69,5 +75,24 @@ public class BucketPickerView extends LinearLayout {
 
     public long getTime() {
         return mCalendar.getTimeInMillis();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "DOWN: x: " + event.getX() + " y: " + event.getY() + " raw x: " + event.getRawX() + " raw y: " + event.getRawY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "MOVE: x: " + event.getX() + " y: " + event.getY() + " raw x: " + event.getRawX() + " raw y: " + event.getRawY());
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "UP: x: " + event.getX() + " y: " + event.getY() + " raw x: " + event.getRawX() + " raw y: " + event.getRawY());
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "CANCEL: x: " + event.getX() + " y: " + event.getY() + " raw x: " + event.getRawX() + " raw y: " + event.getRawY());
+                break;
+        }
+        return true;
     }
 }
